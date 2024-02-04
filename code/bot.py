22441de6e6e455d032c1c184eac9fab92ebbf975
LegendaryBodyGuard <mangatvik@gmail.com>
2024-02-04 10:27:30 -0800

    Bot can now produce the degree and cloud status

--- code/bot.py
+++ code/bot.py
@@ -2,18 +2,21 @@
 import os
 import discord
 import random
+import pyowm
 
 from discord.ext import commands
 from dotenv import load_dotenv
 
-# from pyowm import OWM
-# from pyowm.utils import config
-# from pyowm.utils import timestamps
+# Weather Bots api
+from pyowm import OWM
+from pyowm.utils import config
+from pyowm.utils import timestamps
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
-WEATHER = os.getenv('DISCORD_WEATHER')
+WEATHER = pyowm.OWM(os.getenv('DISCORD_WEATHER'))
+
 
 intents = discord.Intents.default()
 intents.members = True
@@ -46,10 +49,28 @@ bot = commands.Bot(command_prefix='!',intents = discord.Intents.all())
 async def on_ready():
     print(f'{bot.user.name} has connected to Discord!')
 
-# @bot.command(name='weather', help='Lets you know the weather')
-# async def weather(ctx):
-#     print("test")
-#     await ctx.send(response)
+@bot.command(name='weather', help='Lets you know the weather. usage example city,State')
+async def getweatherInfo(ctx,location):
+    mgr = WEATHER.weather_manager()
+    observation = mgr.weather_at_place(location)
+    # observation = mgr.weather_at_place('London,GB')
+    w = observation.weather
+
+    # w.detailed_status,         # 'clouds'
+    # w.wind(),                  # {'speed': 4.6, 'deg': 330}
+    # w.humidity,                # 87
+    # w.temperature('celsius'),  # {'temp_max': 10.5, 'temp': 9.7, 'temp_min': 9.0}
+    # w.rain,                    # {}
+    # w.heat_index,              # None
+    # w.clouds                  # 75
+    # w.weather
+    temp = w.temperature(unit='fahrenheit')
+    status = w.detailed_status
+
+    await ctx.send(f"The weather in {location} is " + str(int(temp['temp'])) + " degrees and " + status)
+    # Will it be clear tomorrow at this time in Milan (Italy) ?
+    # forecast = mgr.forecast_at_place('Milan,IT', 'daily')
+    # answer = forecast.will_be_clear_at(timestamps.tomorrow())
 
 ###############################################################################
 #DEMO NOT GOING TO BE PART OF FEATURE                                         #
